ba52d0797962ea0d2f8f8f5d6d3914cc47ce3c63
Ayan Bhattacharjee <ayanbhattacharjee2004@gmail.com>
2024-07-28 16:21:16 +0530

    remove navbar from login page

--- Food-App/src/App.jsx
+++ Food-App/src/App.jsx
@@ -1,4 +1,5 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import React from "react";
+import { Route, BrowserRouter, Routes, useLocation } from "react-router-dom";
 import Home from "./components/Navber/Home/Home";
 import OurMenu from "./components/Navber/Our menu/OurMenu";
 import Contact from "./components/Navber/Contact/Contact";
@@ -7,21 +8,34 @@ import Login from "./components/Navber/Login/Login";
 import Navber from "./components/Navber/Navber";
 import "./App.css";
 import '@fortawesome/fontawesome-free/css/all.min.css'; // Import Font Awesome CSS
+import Signup from "./components/Navber/Signup/Signup";
 
 function App() {
-    
+  return (
+    <BrowserRouter>
+      <Main />
+    </BrowserRouter>
+  );
+}
+
+function Main() {
+  const location = useLocation();
+
+  // Exclude navbar on login and signup pages
+  const noNavBarRoutes = ["/login", "/signup"];
+  const showNavBar = !noNavBarRoutes.includes(location.pathname.toLowerCase());
+
   return (
     <>
-      <BrowserRouter>
-      <Navber></Navber>
-        <Routes>
+      {showNavBar && <Navber />}
+      <Routes>
         <Route path="/Home" element={<Home />} />
         <Route path="/OurMenu" element={<OurMenu />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/About" element={<About />} />
         <Route path="/Login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   );
 }
